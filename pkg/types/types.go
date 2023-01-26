b02e69c01bc904475314046f935473051820ae8f
Jaborov-U <umed.jaborov.zz@gmail.com>
2023-01-26 18:33:58 +0500

    добавлен types.go

---
+++ pkg/types/types.go
@@ -0,0 +1,47 @@
+package types
+
+// Money = денежная сумма в минимальных единицах
+type Money int64
+
+// Currency = представляет код валюты
+type Currency string
+
+// Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+	EUR Currency = "EUR"
+)
+
+// PAN = номер карты
+type PAN string
+
+// Category представляет собой категорию, в которой был совершен платеж ("авто", "интернет" и т.д.)
+type Category string
+
+// Card = информация о платежной карте
+type Card struct{
+	ID int
+	PAN PAN
+	Balance Money
+	Currency Currency
+	Color string
+	Name string
+	Active bool
+	MinBalance Money
+}
+
+// Payment пресдавляет информацию о платеже
+type Payment struct {
+	ID int
+	Amount Money
+	Category Category
+}
+
+// Слайс из карт для ДЗ 9
+type PaymentSourse struct{
+	Type string // card
+	Number string // номер вида '5058 xxxx xxxx 8888'
+	Balance Money // баланс в дирамах
+}
